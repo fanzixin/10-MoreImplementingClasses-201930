6512849ef7fd32f675dd1d6055e0eab9d864fcba
fanz1 <fanz1@rose-hulman.edu>
2019-03-25 18:43:26 -0400

    Done

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -3,8 +3,8 @@ A   Baby   class and functions that use/test it.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Zixin Fan.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -107,6 +107,24 @@ def main():
 #          """ Brief description of what an object of the class 'is'. """
 #
 ###############################################################################
+class Baby(object):
+    def __init__(self, name):
+        self.name = name
+        print('Hello baby ' + self.name + '!')
+        self.hours = 1
+
+    def feed_baby(self):
+        print('Thank you for feeding baby ' + self.name + '.')
+
+    def hour_passes(self):
+        if self.hours == 1:
+            print('Baby ' + self.name + ' is sleeping.')
+        elif self.hours == 2:
+            print('Baby ' + self.name + ' is awake.  Time for food.')
+        else:
+            print('Baby ' + self.name + ' is CRYING uncontrollably!  Feed the Baby!')
+
+        self.hours = self.hours + 1
 
 
 # -----------------------------------------------------------------------------
